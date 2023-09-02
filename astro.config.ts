e605502e4cd68c21ab24c35d648aad704372bb9d
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2023-09-02 16:47:54 +0100

    upgrade to astro 3.0. added transitions

--- astro.config.ts
+++ astro.config.ts
@@ -1,35 +1,31 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://zizouhuweidi.github.io',
-  markdown: {
-    shikiConfig: {
-      theme: "dark-plus",
-      wrap: true,
-    },
-  },
-  integrations: [
-    mdx({}),
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    sitemap(),
-    prefetch(),
-  ],
-  vite: {
-    optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-    },
-  },
+	site: "https://zizouhuweidi.github.io",
+	markdown: {
+		shikiConfig: {
+			theme: "dark-plus",
+			wrap: true,
+		},
+	},
+	integrations: [
+		mdx({}),
+		tailwind({
+			config: {
+				applyBaseStyles: false,
+			},
+		}),
+		sitemap(),
+		prefetch(),
+	],
+	vite: {
+		optimizeDeps: {
+			exclude: ["@resvg/resvg-js"],
+		},
+	},
 });
